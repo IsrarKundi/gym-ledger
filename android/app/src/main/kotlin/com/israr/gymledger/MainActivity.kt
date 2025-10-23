30fe4a52e7e9cebc101be248dbecf177eb648acc
israrKundi <israrikhere@gmail.com>
2025-10-24 02:17:25 +0500

    firebase configured

--- android/app/src/main/kotlin/com/israr/gymledger/MainActivity.kt
+++ android/app/src/main/kotlin/com/israr/gymledger/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.hbd
+package com.israr.gymledger
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'app/routes/app_pages.dart';
 import 'core/theme/app_theme.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
